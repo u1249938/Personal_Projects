cb9e514e8953486f234295ffbbacfd4993fd9c2c
u1249938 <u1249938@utah.edu>
2022-05-26 09:59:05 -0600

    finished the port scanner

--- Port_Scanner/portScanner.py
+++ Port_Scanner/portScanner.py
@@ -1,7 +1,11 @@
 import socket
+import threading
+from queue import Queue
 
 # Set to local host
 target = '127.0.0.1'
+que = Queue()
+open_ports = []
 
 def portscan(port):
     try:
@@ -11,5 +15,30 @@ def portscan(port):
     except:
         return False
 
-for port in range(1, 1024):
-    print("Port {} is open: ".format(port) + "{}".format(portscan(port)))
+def fill_queue(port_list):
+    for port in port_list:
+        que.put(port)
+
+def worker():
+    while not que.empty():
+        port = que.get()
+        if portscan(port):
+            print("Port {} is open".format(port))
+            open_ports.append(port)
+
+port_list = range(1, 1024)
+fill_queue(port_list)
+
+thread_list = []
+
+for t in range(100):
+    thread = threading.Thread(target=worker)
+    thread_list.append(thread)
+
+for thread in thread_list:
+    thread.start()
+
+for thread in thread_list:
+    thread.join()
+
+print("Open ports are: ", open_ports)
