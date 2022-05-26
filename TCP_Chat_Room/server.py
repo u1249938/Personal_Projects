f75a0ebb272250d97bd4c2ef0809a49941997274
u1249938 <u1249938@utah.edu>
2022-05-26 10:21:36 -0600

    made the server

---
+++ TCP_Chat_Room/server.py
@@ -0,0 +1,51 @@
+import threading
+import socket
+
+host = '127.0.0.1'
+port = 55555 # can be anything greater than 1024
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            # this will not throw an error if client is not sending anything. Only when the client
+            # disconnects
+            message = client.recv(1024) # 1024 is the number of bytes
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of the clilent is {nickname}')
+        broadcast(f'{nickname} joined the chat'.encode('ascii'))
+        client.send('Connected to the server'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
